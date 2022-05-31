2ea64fa411b4728503b0ec092119a912f905cc8c
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-05-31 19:48:33 +0600

    first commit

---
+++ remainder.js
@@ -0,0 +1,18 @@
+
+// vagsesh & vagfol _________________hihi ki mojaaa 
+var mangos = 40;
+var buyingPerson = 3;
+var remainder = mangos % buyingPerson;
+console.log(remainder);
+
+
+// vagsesh & vagfol _________________hihi ki mojaaa 
+
+var mangos = 41;
+var buyingPerson = 3;
+var dividing = mangos / buyingPerson
+console.log(dividing);
+var remainder = mangos % buyingPerson;
+console.log(remainder);
+
+
